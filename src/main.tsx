7352972b44862f914c54a1eef05c00c02f12405d
spde3289 <spde3289@naver.com>
2024-07-01 23:02:20 +0900

    faet: 우클릭 이벤트 추가 우클릭 이벤트 커스텀 훅 우클릭 메뉴 커스텀 컴포넌트

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./style/reset.css";
+import "./style/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
