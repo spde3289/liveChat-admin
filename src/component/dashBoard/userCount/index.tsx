a47cfa350348cd45e3396a4b41f279b072602ba0
spde3289 <spde3289@naver.com>
2024-07-02 15:33:17 +0900

    feat: userCount Data 추가

--- src/component/dashBoard/userCount/index.tsx
+++ src/component/dashBoard/userCount/index.tsx
@@ -1,12 +1,17 @@
 import UserCount from "./userCount";
+import userCountData from "@/data/userCount.json";
 
 const UserCountContainer = () => {
   return (
     <>
-      <UserCount title="메이플 봇 이용 수" count={50} dailyIncrease={0} />
-      <UserCount title="몬헌 봇 이용 수" count={50} dailyIncrease={1} />
-      <UserCount title="문의 방문자 수" count={250} dailyIncrease={1} />
-      <UserCount title="블로그 방문자 수" count={150} dailyIncrease={1} />
+      {userCountData.map((el) => (
+        <UserCount
+          key={el.title}
+          title={el.title}
+          count={el.count}
+          dailyIncrease={el.dailyIncrease}
+        />
+      ))}
     </>
   );
 };
